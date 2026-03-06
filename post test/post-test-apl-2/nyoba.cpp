528f59a26a6db6fb6a3718f665777cc57092ece3
Nopal07A <mnaufalal101@gmail.com>
2026-03-06 11:54:09 +0800

    apl pt 2

---
+++ post test/post-test-apl-2/nyoba.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using namespace std;
+
+struct Hero{
+    string nama;
+    string role;
+    string kesulitan;
+};
+
+Hero hero[100] = {
+    {"Ling","Assassin","Hard"},
+    {"Tigreal","Tank","Easy"},
+    {"Kagura","Mage","Hard"},
+    {"Beatrix","Marksman","Medium"},
+    {"Fredrinn","Fighter","Medium"}
+};
+
+int jumlahHero = 5;
+
+bool login(){
+
+    string username, password;
+    int percobaan = 0;
+
+    while(percobaan < 3){
+
+        cout<<"=== LOGIN ==="<<endl;
+
+        cout<<"Masukkan Nama : ";
+        cin>>username;
+
+        cout<<"Masukkan NIM  : ";
+        cin>>password;
+
+        if(username == "050" && password == "050"){
+            cout<<"Login berhasil!\n";
+            return true;
+        }
+
+        percobaan++;
+
+        cout<<"Login gagal! Sisa percobaan "<<3-percobaan<<endl;
+
+    }
+
+    return false;
+}
+
+int tambahHero(){
+
+    cout<<"\n=== TAMBAH HERO ==="<<endl;
+
+    cout<<"Nama Hero : ";
+    cin>>hero[jumlahHero].nama;
+
+    cout<<"Role Hero : ";
+    cin>>hero[jumlahHero].role;
+
+    cout<<"Tingkat Kesulitan (Easy/Medium/Hard): ";
+    cin>>hero[jumlahHero].kesulitan;
+
+    jumlahHero++;
+
+    return 0;
+}
+
+int lihatHero(){
+
+    cout<<"\n=== DATA HERO ==="<<endl;
+
+    for(int i=0;i<jumlahHero;i++){
+
+        cout<<i+1<<". "
+            <<hero[i].nama<<" | "
+            <<hero[i].role<<" | "
+            <<"Kesulitan: "<<hero[i].kesulitan<<endl;
+
+    }
+
+    return 0;
+}
+
+int ubahHero(){
+
+    int no;
+
+    lihatHero();
+
+    cout<<"Pilih nomor hero yang diubah : ";
+    cin>>no;
+
+    no--;
+
+    cout<<"Nama baru : ";
+    cin>>hero[no].nama;
+
+    cout<<"Role baru : ";
+    cin>>hero[no].role;
+
+    cout<<"Kesulitan baru : ";
+    cin>>hero[no].kesulitan;
+
+    return 0;
+}
+
+int hapusHero(){
+
+    int no;
+
+    lihatHero();
+
+    cout<<"Pilih nomor hero yang dihapus : ";
+    cin>>no;
+
+    no--;
+
+    for(int i=no;i<jumlahHero;i++){
+        hero[i] = hero[i+1];
+    }
+
+    jumlahHero--;
+
+    return 0;
+}
+
+int main(){
+
+    if(login()==false){
+
+        cout<<"Program berhenti karena gagal login 3 kali";
+        return 0;
+    }
+
+    int pilih;
+
+    do{
+
+        cout<<"\n=== MENU HERO MOBILE LEGENDS ==="<<endl;
+        cout<<"1. Tambah Hero"<<endl;
+        cout<<"2. Lihat Hero"<<endl;
+        cout<<"3. Ubah Hero"<<endl;
+        cout<<"4. Hapus Hero"<<endl;
+        cout<<"5. Keluar"<<endl;
+
+        cout<<"Pilih : ";
+        cin>>pilih;
+
+        switch(pilih){
+
+            case 1: tambahHero(); break;
+            case 2: lihatHero(); break;
+            case 3: ubahHero(); break;
+            case 4: hapusHero(); break;
+            case 5: cout<<"Program selesai"<<endl; break;
+
+        }
+
+    }while(pilih != 5);
+
+    return 0;
+}
